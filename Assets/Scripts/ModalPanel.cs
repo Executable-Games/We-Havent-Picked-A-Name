bd27e9493f53df40c9510c4fe56c3a959299fb7e
dabomb2654 <hd@u.northwestern.edu>
2016-02-07 17:26:00 -0600

    dialogue boxes over motion

---
+++ Assets/Scripts/ModalPanel.cs
@@ -0,0 +1,198 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+
+public class EventButtonDetails
+{
+    public string buttonTitle;
+    public Sprite buttonBackground;
+    public UnityAction action;
+}
+
+public class ModalPanelDetails
+{
+    public string title;
+    public string question;
+    public Sprite iconImage;
+    public Sprite panelBackground;
+    public EventButtonDetails button1Details;
+    public EventButtonDetails button2Details;
+    public EventButtonDetails button3Details;
+}
+
+public class ModalPanel : MonoBehaviour {
+
+    public Text question;
+    public Image iconImage;
+    public Button opt1Button;
+    public Button opt2Button;
+    public Button opt3Button;
+
+    public Text opt1Text;
+    public Text opt2Text;
+    public Text opt3Text;
+
+    public GameObject modalPanelObject;
+
+    private static ModalPanel modalPanel;
+
+    public static ModalPanel Instance()
+    {
+        if (!modalPanel)
+        {
+            modalPanel = FindObjectOfType(typeof(ModalPanel)) as ModalPanel;
+            if (!modalPanel)
+                Debug.LogError("There needs to be one active ModalPanel script on a GameObject in your scene.");
+        }
+
+        return modalPanel;
+    }
+
+
+    // how to choose an option on the dialogue box
+    // UnityAction - pointer to a function
+    public void PlayerGenChoice(ModalPanelDetails details)
+    {
+        modalPanelObject.SetActive(true);
+
+        this.iconImage.gameObject.SetActive(false);
+        opt1Button.gameObject.SetActive(false);
+        opt2Button.gameObject.SetActive(false);
+        opt3Button.gameObject.SetActive(false);
+
+        this.question.text = details.question;
+
+        if (details.iconImage)
+        {
+            this.iconImage.sprite = details.iconImage;
+            this.iconImage.gameObject.SetActive(true);
+        }
+
+        opt1Button.onClick.RemoveAllListeners();
+        opt1Button.onClick.AddListener(details.button1Details.action);
+        opt1Button.onClick.AddListener(ClosePanel);
+        opt1Text.text = details.button1Details.buttonTitle;
+        opt1Button.gameObject.SetActive(true);
+
+        if (details.button2Details != null)
+        {
+            opt2Button.onClick.RemoveAllListeners();
+            opt2Button.onClick.AddListener(details.button2Details.action);
+            opt2Button.onClick.AddListener(ClosePanel);
+            opt2Text.text = details.button2Details.buttonTitle;
+            opt2Button.gameObject.SetActive(true);
+        }
+
+        if (details.button3Details != null)
+        {
+            opt3Button.onClick.RemoveAllListeners();
+            opt3Button.onClick.AddListener(details.button3Details.action);
+            opt3Button.onClick.AddListener(ClosePanel);
+            opt3Text.text = details.button3Details.buttonTitle;
+            opt3Button.gameObject.SetActive(true);
+        }
+    }
+    
+
+    public void PlayerChoice(string question, UnityAction opt3, Sprite iconImage = null)
+    {
+        modalPanelObject.SetActive(true);
+        opt3Button.onClick.RemoveAllListeners();
+        opt3Button.onClick.AddListener(opt3);
+        opt3Button.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+        if (iconImage)
+            this.iconImage.sprite = iconImage;
+
+        if (iconImage) 
+            this.iconImage.gameObject.SetActive(true);
+        else
+            this.iconImage.gameObject.SetActive(false);
+
+        opt1Button.gameObject.SetActive(false);
+        opt2Button.gameObject.SetActive(false);
+        opt3Button.gameObject.SetActive(true);
+
+    }
+    public void PlayerChoice(string question, UnityAction opt1, UnityAction opt2, UnityAction opt3)
+    {
+        modalPanelObject.SetActive(true);
+
+        opt1Button.onClick.RemoveAllListeners();
+        opt1Button.onClick.AddListener(opt1);
+        opt1Button.onClick.AddListener(ClosePanel);
+
+        opt2Button.onClick.RemoveAllListeners();
+        opt2Button.onClick.AddListener(opt2);
+        opt2Button.onClick.AddListener(ClosePanel);
+
+        opt3Button.onClick.RemoveAllListeners();
+        opt3Button.onClick.AddListener(opt3);
+        opt3Button.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+
+        this.iconImage.gameObject.SetActive(false);
+        opt1Button.gameObject.SetActive(true);
+        opt2Button.gameObject.SetActive(true);
+        opt3Button.gameObject.SetActive(true);
+    }
+
+
+    // how to choose an option on the dialogue box (with an icon)
+    public void PlayerChoice(string question, Sprite iconImage, UnityAction opt1, UnityAction opt2, UnityAction opt3)
+    {
+        modalPanelObject.SetActive(true);
+
+        opt1Button.onClick.RemoveAllListeners();
+        opt1Button.onClick.AddListener(opt1);
+        opt1Button.onClick.AddListener(ClosePanel);
+
+        opt2Button.onClick.RemoveAllListeners();
+        opt2Button.onClick.AddListener(opt2);
+        opt2Button.onClick.AddListener(ClosePanel);
+
+        opt3Button.onClick.RemoveAllListeners();
+        opt3Button.onClick.AddListener(opt3);
+        opt3Button.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+        this.iconImage.sprite = iconImage;
+
+        this.iconImage.gameObject.SetActive(true);
+        opt1Button.gameObject.SetActive(true);
+        opt2Button.gameObject.SetActive(true);
+        opt3Button.gameObject.SetActive(true);
+
+    }
+
+    public void PlayerChoice(string question, UnityAction opt1, UnityAction opt2)
+    {
+        modalPanelObject.SetActive(true);
+
+        opt1Button.onClick.RemoveAllListeners();
+        opt1Button.onClick.AddListener(opt1);
+        opt1Button.onClick.AddListener(ClosePanel);
+
+        opt2Button.onClick.RemoveAllListeners();
+        opt2Button.onClick.AddListener(opt2);
+        opt2Button.onClick.AddListener(ClosePanel);
+
+        this.question.text = question;
+
+        this.iconImage.gameObject.SetActive(false);
+        opt1Button.gameObject.SetActive(true);
+        opt2Button.gameObject.SetActive(true);
+        opt3Button.gameObject.SetActive(false);
+
+
+    }
+
+    void ClosePanel()
+    {
+        modalPanelObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/TestModalWindow.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class TestModalWindow : MonoBehaviour {
+
+    public Sprite icon;
+    public Transform spawnPoint;
+
+    public GameObject thingToSpawn;
+
+    private ModalPanel modalPanel;
+    private DisplayManager displayManager;
+
+    private UnityAction myOpt1Action;
+    private UnityAction myOpt2Action;
+    private UnityAction myOpt3Action;
+    private UnityAction myOpt4Action;
+
+    void Awake()
+    {
+        modalPanel = ModalPanel.Instance();
+        displayManager = DisplayManager.Instance();
+
+        //myOpt1Action = new UnityAction(TestOpt1);
+        //myOpt2Action = new UnityAction(TestOpt2);
+        //myOpt3Action = new UnityAction(TestOpt3);
+    }
+
+    //tell modal panel to set up buttons and functions to call
+    public void TestOptions()
+    {
+        //modalPanel.PlayerChoice("Which Option would you like?\n Or something else perhaps?", myOpt1Action, myOpt2Action, myOpt3Action);
+        modalPanel.PlayerChoice("Which Option would you like?\n Or something else perhaps?", TestOpt1, TestOpt2, TestOpt3);
+    }
+
+    //using PlayerGenChoice
+
+    public void TestGenChoice()
+    {
+        ModalPanelDetails modalPanelDetails = new ModalPanelDetails {question = "This is an announcement!\nIf you don't like it, shove off!" };
+        modalPanelDetails.button1Details = new EventButtonDetails{buttonTitle = "Gotcha!", action = TestOpt3};
+
+        modalPanel.PlayerGenChoice(modalPanelDetails);
+    }
+    public void TestOptions3WithImage()
+    {
+        modalPanel.PlayerChoice("Which Option would you like?\n Or something else perhaps?", icon, TestOpt1, TestOpt2, TestOpt3);
+    }
+
+    public void TestLambda()
+    {
+        ModalPanelDetails modalPanelDetails = new ModalPanelDetails { question = "Do you want to create three things?" }; modalPanelDetails.button1Details = new EventButtonDetails
+        {
+            buttonTitle = "Yes Please!",
+            action = () => { InstantiateObject(thingToSpawn); InstantiateObject(thingToSpawn, thingToSpawn); }
+        };
+        modalPanelDetails.button2Details = new EventButtonDetails
+        {
+            buttonTitle = "No thanks!",
+            action = TestOpt2
+        };
+
+        modalPanel.PlayerGenChoice(modalPanelDetails);
+    }
+
+    //can double up the number of spawns by repeating lambda call: () => { InstantiateObject(thingToSpawn); InstantiateObject(thingToSpawn);}, TestOpt2);
+
+    public void TestLambda2()
+    {
+        modalPanel.PlayerChoice("Do you want to spawn an object?", () => { InstantiateObject(thingToSpawn); }, TestOpt2);
+    }
+
+    void TestOpt1()
+    {
+        displayManager.DisplayMessage("WOO!");
+    }
+    void TestOpt2()
+    {
+        displayManager.DisplayMessage("Option2!");
+    }
+    void TestOpt3()
+    {
+        displayManager.DisplayMessage("HEEEY");
+    }
+
+    void InstantiateObject(GameObject thingToInstantiate)
+    {
+        displayManager.DisplayMessage("Here you go!");
+        Instantiate(thingToInstantiate, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    void InstantiateObject(GameObject thingToInstantiate, GameObject thingToInstantiate2)
+    {
+        displayManager.DisplayMessage("Made a thing!");
+        Instantiate(thingToInstantiate, spawnPoint.position - Vector3.one, spawnPoint.rotation);
+        Instantiate(thingToInstantiate2, spawnPoint.position + Vector3.one, spawnPoint.rotation);
+    }
+}
