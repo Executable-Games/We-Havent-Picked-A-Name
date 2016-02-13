2b4be9053d57578e0c43539f90f9190dca3d86f4
skorlir <skorlir@gmail.com>
2016-02-13 17:19:39 -0600

    IAttack interface; closes #11

--- Assets/Scripts/Combat/Attacks/IAttack.cs
+++ Assets/Scripts/Combat/Attacks/IAttack.cs
@@ -3,43 +3,37 @@
 /// </summary>
 /// Author: Jordan (GitHub: @skorlir)
 /// 2/13/16
-abstract class IAttack {
+interface IAttack {
     /// <summary>
     /// Amount of Damage an Attack will do
     /// </summary>
-    public abstract float Damage {
-        get;
-    }
+    public float Damage;
 
     /// <summary>
     /// The Accuracy of an Attack
     /// </summary>
-    public abstract float Accuracy {
-        get;
-    }
+    public float Accuracy;
 
     /// <summary>
     /// What should be displayed on-screen as the name of the Attack
     /// </summary>
-    public abstract string DisplayName {
-        get;
-    }
+    public string DisplayName;
 
     /// <summary>
     /// Whether the Attack is currently allowed in combat
     /// </summary>
-    public bool Allowed = true;
+    public bool Allowed;
 
     /// <summary>
     /// Set whether an Attack is currently allowed
     /// </summary>
     /// <param name="allowed">Value to set</param>
-    abstract public void SetAllowed (bool allowed);
+    public void SetAllowed (bool allowed);
 
     /// <summary>
     /// Perform the Attack
     /// </summary>
     /// <param name="target">Unit the Attack will be performed on</param>
     /// <returns>The amount of damage done by the attack</returns>
-    abstract public float PerformAttack (Unit target);
+    public float PerformAttack (Unit target);
 }
