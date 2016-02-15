a35c2d5a43134fa14a2fa7b6f1782b5a6a82af12
Sarah Miller <SMiller2016@u.northwestern.edu>
2016-02-14 18:51:43 -0600

    Created basic attack script
    A basic implementation of the Attack interface, as a Component that can
    be placed on a Unit

---
+++ Assets/Scripts/Combat/Attacks/BasicAttack.cs
@@ -0,0 +1,33 @@
+﻿/// <summary>
+/// Basic Attack
+/// </summary>
+/// Author: Sarah (GitHub: @skim74)
+/// 2/14/16
+
+public class BasicAttack : IAttack {
+	/// <summary>
+	/// Does 1hp damage
+	/// </summary>
+	public override float Damage {
+		get { 
+			return 1f;
+		}
+	}
+	/// <summary>
+	/// Hits 100% of the time
+	/// </summary>
+	public override float Accuracy {
+		get { 
+			return 1f;
+		}
+	}
+
+	/// <summary>
+	/// Displays "Basic Attack" at name of attack
+	/// </summary>
+	public override string DisplayName {
+		get { 
+			return "Basic Attack";
+		}
+	}
+}
