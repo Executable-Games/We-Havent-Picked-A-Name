5cb8ae51f708dc64c40624f8596e4d4b69928971
Aaron Karp <aaronkarp123@gmail.com>
2016-02-17 17:22:46 -0600

    Added camera following script to be attached to MainCamera. All else can be ignored.

---
+++ Assets/Scripts/CameraRunner.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRunner : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+
+    void start () {
+    }
+
+    void Update () 
+    {
+        transform.position = new Vector3 (player.position.x + offset.x, 0, offset.z); // Camera follows the player with specified offset position
+    }
+}
