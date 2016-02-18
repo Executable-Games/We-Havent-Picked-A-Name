8ce041490948028d9543d7d82280ccc992c69c7a
Aaron Karp <aaronkarp123@gmail.com>
2016-02-18 12:42:18 -0600

    Added comments to CameraRunner script.

--- Assets/Scripts/CameraRunner.cs
+++ Assets/Scripts/CameraRunner.cs
@@ -1,9 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Class for Camera to Follow Player
+/// </summary>
+/// Author: Aaron (GitHub: @aaronkarp123)
+/// 2/16/2016
 public class CameraRunner : MonoBehaviour {
 
+    /// <summary>
+    /// Character for camera to follow
+    /// </summary>
     public Transform player;
+
+    /// <summary>
+    /// Vector to add an offset to the following motion (should default to 0)
+    /// </summary>
     public Vector3 offset;
 
     void start () {
