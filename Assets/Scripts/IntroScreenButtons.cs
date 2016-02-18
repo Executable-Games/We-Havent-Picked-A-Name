682f2635e7c31246534d31b31a83e83b203af9a9
dabomb2654 <hd@u.northwestern.edu>
2016-02-18 15:02:21 -0600

    load start of game or quit

---
+++ Assets/Scripts/IntroScreenButtons.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
+
+public class IntroScreenButtons : MonoBehaviour {
+
+
+	public void OnClickLoad(){
+
+		SceneManager.LoadScene (1);
+	}
+	public void OnClickExit(){
+		Application.Quit ();
+	}
+
+}
