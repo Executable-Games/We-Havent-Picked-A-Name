71920ae25931a854eb8ac35ab424b01a76fd78b4
dabomb2654 <hd@u.northwestern.edu>
2016-02-18 14:58:22 -0600

    uses transforms in update to move g.o.
    This is for cutscenes, not player interaction. *temp stopgap*

---
+++ Assets/Scripts/AutoMoveForCutscene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoMoveForCutscene : MonoBehaviour {
+
+	public float speed;
+	public float stopRight;
+	public float stopUp;
+
+	void Start(){
+
+	}
+	void Update () {
+	
+		if (transform.localPosition.x <= stopRight)
+			MoveRight ();
+		if (transform.localPosition.y <= stopUp)
+			MoveUp ();
+			
+	}
+
+	void MoveRight(){
+		transform.localPosition += transform.right * speed * Time.deltaTime;
+
+	}
+		
+	void MoveUp(){
+		transform.localPosition += transform.up * speed * Time.deltaTime;
+	}
+}
