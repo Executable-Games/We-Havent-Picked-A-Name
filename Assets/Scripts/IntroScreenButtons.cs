99402f0414fce4cb3da8184fc0d37de00bbbdfcd
dabomb2654 <hd@u.northwestern.edu>
2016-02-21 19:25:26 -0600

    buttons for title screen

--- Assets/Scripts/IntroScreenButtons.cs
+++ Assets/Scripts/IntroScreenButtons.cs
@@ -1,15 +1,17 @@
 ﻿using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; //use this library to enable SceneManager
 using UnityEngine.UI;
 using System.Collections;
 
+/// <summary>
+/// start and exit buttons for title screen
+/// </summary>
 public class IntroScreenButtons : MonoBehaviour {
 
-
 	public void OnClickLoad(){
-
 		SceneManager.LoadScene (1);
 	}
+	//only works in builds, not in the editor
 	public void OnClickExit(){
 		Application.Quit ();
 	}
